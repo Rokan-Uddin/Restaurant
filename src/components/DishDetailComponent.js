3ea86ef779c3a2f36c3678ddb0d201999d6f5e81
Rokan Uddin <rokanu55@gmail.com>
2020-07-16 10:27:52 +0600

    assignment,added commentForm

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,7 +1,66 @@
- import React from 'react';
+ import React,{Component} from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem,Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label  } from 'reactstrap';
 import { Link } from 'react-router-dom';
+    
+    class CommentForm extends Component {
+        constructor(props) {
+            super(props);
+            this.toggleModal= this.toggleModal.bind(this);
+            this.handleLogin = this.handleLogin.bind(this);
+            this.state= { isModelOpen:false };
+        }
+        toggleModal() {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen });
+        }
+        handleLogin(event) {
+        this.toggleModal();
+        alert("Your Name: " + this.username.value + "Rating : " + this.rating.value +
+             "Comments: " + this.comment.value);
+        event.preventDefault();
+        }
+        render(){
+            return(  <div>
+                <Button outline onClick={this.toggleModal} ><span className="fa fa-pencil fa-lg"></span> Submit Comments</Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                    <Form onSubmit={this.handleLogin} >
+                            <FormGroup>
+                                <Label for="rating">Rating</Label>
+                                <Input type="select" name="rating" id="rating"
+                                innerRef={(input) => this.rating= input}
+                                 >
+                                  <option>1</option>
+                                  <option>2</option>
+                                  <option>3</option>
+                                  <option>4</option>
+                                  <option>5</option>
+                                </Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="username">Your Name</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input}
+                                    placeholder="Your Name" />
+                            </FormGroup>
+                            <FormGroup >
+                                    <Label htmlFor="comment">Comment </Label>
+                                    <Input rows="6" type="textarea" id="comment" name="comment"
+                                    innerRef={(input)=> this.comment=input}
+                                    placeholder="comment" />
+                            </FormGroup>
+            
+                            <Button type="submit" value="submit" color="primary">Submit</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+                </div>
+             );
+        }
+    }
 
     function RenderComments({comments}) {
         if (comments == null) {
@@ -28,7 +87,7 @@ import { Link } from 'react-router-dom';
                 <ul className='list-unstyled'>
                     {cmnts}
                 </ul>
-
+                < CommentForm />
             </div>
         )
     }
