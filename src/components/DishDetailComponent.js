0c31bc29077b01dda6efa4f7e247021c575ef941
Rokan Uddin <rokanu55@gmail.com>
2020-07-17 08:20:51 +0600

    Combining Reducers

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,7 +1,6 @@
  import React,{Component} from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem,Button, Modal, ModalHeader, ModalBody,FormFeedback,
-    Form, FormGroup, Input, Label,Col,Row  } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem,Button, Modal, ModalHeader, ModalBody, Label,Col,Row  } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 const required = (val) => val && val.length;
